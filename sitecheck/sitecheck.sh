e0c5a8ee9b98fdfe44ed66648cc7a66194729dd2
karma <captainav0608@gmail.com>
2025-05-22 00:24:04 +0530

    update error handling for exi code 6
    Signed-off-by: karma <captainav0608@gmail.com>

--- sitecheck/sitecheck.sh
+++ sitecheck/sitecheck.sh
@@ -1,23 +1,37 @@
+function error_dictionary {
+    case $1 in
+        "0") echo "all good";;
+        "6") echo "unresolved hostname";;
+        *) echo "some other error"
+    esac
+}
 function code_dictionary {
     case $1 in 
         "200") echo "OK";;
+        "301") echo "Moved Permanently";;
+        "400") echo "Bad request";;
+        "401") echo "Unauthorized";;
         "403") echo "Forbidden";;
         "404") echo "Not Found";;
+        "408") echo "Request Timeout";;
+        "000") echo $(error_dictionary $2);;
         *) echo "Error"
     esac
 }
 
+
 list="./list.txt"
 
 response_code=0
+curl_exit_code=0
 
 echo "\n \n"
 for item in $(cat $list)
 do
-    response_code=$(curl -s -w "%{http_code}" -I $item -o /dev/null)    
+    response_code=$(curl -s -w "%{http_code}" -I "$item" -o /dev/null)    
+    curl_exit_code=$?
     echo "Site : $item" 
-    # echo $(curl -I $item -s | grep "HTTP")
-    desc=$(code_dictionary $response_code)
+    desc=$(code_dictionary $response_code $curl_exit_code)
     echo "Status - $response_code ( $desc )"
     echo "----------------------------"
 done
