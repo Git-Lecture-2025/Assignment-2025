f5c6e2fea5ca459c744cd28847b476d7f01d498d
asutosh29 <asutosh29aaz@gmail.com>
2025-05-23 00:15:53 +0530

    Add function to add site to tracklist. Add check if tracklist file exists

--- amx/pinger.sh
+++ amx/pinger.sh
@@ -2,11 +2,32 @@
 
 # Tool used to moniter status of websites
 
+if ! [ -f ./tracklist.txt ]
+then
+    echo "Tracklist doesnt exists"
+    echo "Creating tracklist.txt"
+    touch ./tracklist.txt
+fi
+
+add_site() {
+    if [ -z $(grep $1 ./tracklist.txt) ]
+    then
+        echo "adding $1"
+        echo "$1" >> ./tracklist.txt
+        echo "$1 added successfully"
+    else
+        echo "Site already present in the track list"
+        echo "use -l to view all sites!"
+    fi
+
+    exit 1
+}
+
 while getopts ":pa:d:l" opt
 do
     case $opt in
         p) echo "pinging sites";;
-        a) input=$OPTARG;echo "$input";;
+        a) input=$OPTARG;add_site "$input";;
         d) input=$OPTARG;echo "$input";;
         l) echo "list site";;
         \?) echo "help";exit 1
