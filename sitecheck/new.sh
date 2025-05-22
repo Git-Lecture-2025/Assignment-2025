00a6d8253bfe817fe31eaf12b5973062d6e4b8f9
karma <captainav0608@gmail.com>
2025-05-22 22:10:10 +0530

    add escape sequencing and styling of logo
    Signed-off-by: karma <captainav0608@gmail.com>

---
+++ sitecheck/new.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# figlet "SiteCheck" 
+# lolcat -a -s 100 
+
+echo -e "\033[32m ____  _ _        ____ _               _    \033[0m"
+echo -e "\033[33m/ ___|(_) |_ ___ / ___| |__   ___  ___| | __\033[0m"
+echo -e "\033[36m\___ \| | __/ _ \ |   | '_ \ / _ \/ __| |/ /\033[0m"
+echo -e "\033[36m ___) | | ||  __/ |___| | | |  __/ (__|   < \033[0m"
+echo -e "\033[33m|____/|_|\__\___|\____|_| |_|\___|\___|_|\_\ \033[0m"
+
+
+echo -e "\033[32mThis is green\033[0m"
+echo -e "\033[33mThis is yellow\033[0m"
+echo -e "\033[36mThis is cyan\033[0m"
+echo -e "\033[35mThis is magenta\033[0m"
+echo -e "\033[32mThis is green\033[0m"
