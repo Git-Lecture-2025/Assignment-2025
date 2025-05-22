eccd358c8e188830c07f2c7cd534f50c7dd0c7dd
Tanmay Arya <tanmayarya2018@gmail.com>
2025-05-22 13:45:02 +0530

    Add Basic CLI Skeleton

---
+++ uptime.sh/main.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+help(){
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "Commands:"
+    echo "  help : Show this help message"
+    echo "  add  : Add a new site to monitor"
+    echo "  remove, rm : Remove a site from monitoring"
+    echo "  ls, list : List all monitored sites"
+    echo "  check, ping : Check the status of all monitored sites"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help : Show this help message"
+}
+
+remove_site() {
+
+}
+
+list_sites() {
+
+}
+
+check_sites() {
+
+}
+
+add_site(){
+    local trackerid=$(uuidgen)
+    trackerid=${trackerid:0:8}
+    local name=$1
+    local url=$2
+    local status="unknown"
+    local response_time="inf"
+    local last_checked="never"
+    echo "$trackerid,$name,$url,$status,$response_time",$last_checked >> uptime.csv
+}
+
+case $1 in 
+    help|-h|--help)
+        help
+        ;;
+    add)
+        add_site "$2" "$3"
+        ;;
+    remove|rm)
+        remove_site "$2"
+        ;;
+    ls|list)
+        list_sites
+        ;;
+    check|ping) 
+        check_sites
+        ;;
+    *)
+        echo "Invalid command: $1"
+        help
+        ;;
+esac
