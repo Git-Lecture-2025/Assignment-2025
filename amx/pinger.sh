9e450359ea39fe7460cc12d178f18df8f6a250c9
asutosh29 <asutosh29aaz@gmail.com>
2025-05-23 00:10:44 +0530

    Setup getopts to take options and flags from user

--- amx/pinger.sh
+++ amx/pinger.sh
@@ -2,3 +2,13 @@
 
 # Tool used to moniter status of websites
 
+while getopts ":pa:d:l" opt
+do
+    case $opt in
+        p) echo "pinging sites";;
+        a) input=$OPTARG;echo "$input";;
+        d) input=$OPTARG;echo "$input";;
+        l) echo "list site";;
+        \?) echo "help";exit 1
+    esac
+done
