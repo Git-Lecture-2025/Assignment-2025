b05f48d20104f015b6a82c68d6de821ca1f2d12d
kunal <kunal22102005@gmail.com>
2025-05-24 03:15:23 +0530

    Remove the original functions in tracker.sh

--- tracker/tracker.sh
+++ tracker/tracker.sh
@@ -37,7 +37,7 @@ function track() {
 	fi
 
 	for url in "$@"; do
-		if grep -Fq "$url" "$datafile"; then
+		if grep -q "^$url" "$datafile"; then
 			echo "Already tracking: $url..."
 			return
 		fi
@@ -102,38 +102,6 @@ function untrack() {
 	echo "Untracked ${resolved} arguments successfully."
 	if (( resolved > 0 && resolved != "$#" )); then
 		echo "Failed to resolve remaining arguments..."
-	fi
-}
-
-function getStatus() {
-	local code=$(curl -s -o /dev/null -w "%{http_code} %{time_total}" "$1")
-	IFS=' ' read -r -a code <<< "$code"
-
-	echo "Tracking Result: ${1}; Response Code: ${code[0]}; Latency: ${code[1]}"
-}
-
-function status() {
-	echo "-----Tracking Results------" 
-
-	local lines=$(( $(wc -l < "$datafile") ))
-	if [[ "$#" -eq 0 ]]; then
-        if [[ $lines -eq 0 ]]; then
-            echo "No websites being tracked currently..."
-            return
-        fi
- 
-		while IFS= read -r line; do
-			getStatus "$line"
-		done < "$datafile"
-	else
-		for id in "$@"; do
-			if [[ "$id" -le 0 || "$id" -gt "$lines" ]]; then
-				echo "Invalid ID: ${id}, ignoring..."
-				return
-			fi
-
-			getStatus $(sed -n "${id}p" .datafile.txt)
-		done
 	fi
 }
 
