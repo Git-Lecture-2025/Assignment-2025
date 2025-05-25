041e3fe87c32a8c445d755f429c2205540572e16
Aryan Goyal <goyal.aryan@gmail.com>
2025-05-25 23:25:49 +0530

    [FIX] fixed cancel button of add functionality and removed comments
    Signed-off-by: Aryan Goyal <goyal.aryan@gmail.com>

--- sds_website_tracking.sh
+++ sds_website_tracking.sh
@@ -45,28 +45,34 @@ do
             do
                 same="yes"
                 add=$(dialog --title "Adding a site" --inputbox "Please enter the site that you want to add" 0 0 3>&1 1>&2 2>&3 3>&-)
-                if [ "$length" -ne 0 ]
-                then
-                    while [ "$same" = "yes" ]
-                    do
-                        for items in ${arr[@]}
+                add_exit_code=$?
+                if [ "$add_exit_code" -eq 1 ]
+                    then
+                        y_n="0"
+                        same="no"
+                        check="yes"
+                else
+                    if [ "$length" -ne 0 ]
+                    then
+                        while [ "$same" = "yes" ]
                         do
-                            if [ "$items" = "$add" ]
-                            then
-                                dialog --title "Error" --msgbox "This site has already been added" 0 0
-                                add=$(dialog --title "Adding a site" --inputbox "Please enter the site that you want to add" 0 0 3>&1 1>&2 2>&3 3>&-)
-                                same="yes"
-                            else
-                                same="no"
-                            fi
+                            for items in ${arr[@]}
+                            do
+                                if [ "$items" = "$add" ]
+                                then
+                                    dialog --title "Error" --msgbox "This site has already been added" 0 0
+                                    add=$(dialog --title "Adding a site" --inputbox "Please enter the site that you want to add" 0 0 3>&1 1>&2 2>&3 3>&-)
+                                else
+                                    same="no"
+                                fi
+                            done
                         done
-                    done
+                    fi
                 fi
-                # sc=$(curl -s -o /dev/null -w "%{http_code} ${add}")
-                if [ $? -eq 1 ]
+                if [ $add_exit_code -eq 1 ]
                 then
-                    dialog --title "Cancel" --msgbox "You will meet a yes or no question where if you don't want to add , remove or edit anything then you can press no" 0 0
-                    break
+                    dialog --title "Cancel" --msgbox "You will now return to the home page" 0 0
+                    y_n="1"
                 else
                     status_code=$(curl -L --write-out %{http_code} --silent --output /dev/null "$add")
                     if [ $? -eq 0 ]
@@ -126,11 +132,9 @@ do
                     then
                         echo "URL exists: $url"
                         curl -L -s -o /dev/null -w "Status Code of $url: %{http_code}\n" https://www.example.com >> website.txt
-                        # curl -s -o /dev/null -w "Status Code of $url: %{http_code}\n" https://www.example.com
                     else
                         echo "URL does not exist: $url"
                         curl -L -s -o /dev/null -w "Status Code of $url: %{http_code}\n" https://www.example.com >> website.txt
-                        # curl -s -o /dev/null -w "Status Code of $url: %{http_code}\n" https://www.example.com
                     fi
                 done
             dialog --title status --textbox website.txt 0 0
