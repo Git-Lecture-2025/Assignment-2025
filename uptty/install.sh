a9793421adc14b0668ba31c761fdd74047cca075
Tanmay Arya <tanmayarya2018@gmail.com>
2025-05-25 19:23:55 +0530

    feat: Install script now lets users replace their current installation with the latest version of UpTTY

--- uptty/install.sh
+++ uptty/install.sh
@@ -14,6 +14,21 @@ echo "▐█▄█▌▐█▪·• ▐█▌· ▐█▌· ▐█▀·."
 echo " ▀▀▀ .▀    ▀▀▀  ▀▀▀   ▀ • "
 echo ""
 
+if [ -f /usr/bin/uptty ]; then
+    echo "Looks like you already have UpTTY installed."
+    echo -n "Delete the current installation and install the latest version of UpTTY? (Y/n) "
+    read -rn1 inp
+    echo
+    if [[ "$inp" == "n" || "$inp" == "N" ]]; then
+        echo "Exiting Installer!"
+        exit
+    else 
+        rm -f /usr/bin/uptty
+        rm -f /usr/bin/uptty-tui
+    fi
+fi
+    
+
 echo "Installing UpTTY ..."
 wget -nv --no-cache -O /usr/bin/uptty https://raw.githubusercontent.com/TanmayArya-1p/Assignment-2025/refs/heads/tcan/uptty/uptty
 
@@ -21,5 +36,11 @@ echo "Installing UpTTY TUI ..."
 wget -nv --no-cache -O /usr/bin/uptty-tui https://raw.githubusercontent.com/TanmayArya-1p/Assignment-2025/refs/heads/tcan/uptty/uptty-tui
 
 chmod +x /usr/bin/uptty
-chmod +x /usr/bin/uptty-tu  i
+chmod +x /usr/bin/uptty-tui
 uptty version
+if [[ $? == 0 ]]; then
+    echo "Successfully Installed UpTTY."
+else
+    echo "Something went wrong during installation."
+fi
+
